62afb0b21a58d66788e1fa14e68fc84652e2c7f3
Samneet Singh <samneetdhillon@hotmail.com>
2021-03-11 17:55:10 -0500

    Stack Min: How would you design a stack which, in addition to push and pop, has a function min which returns the minimum element? Push, pop and min should all operate in 0(1) time.

---
+++ test/stacksAndQueues/testMinStack.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestMinStack(TestCase):
+    def test_push(self):
+        self.fail()
+
+    def test_pop(self):
+        self.fail()
+
+    def test_min(self):
+        self.fail()

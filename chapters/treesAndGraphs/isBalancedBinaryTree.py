537ab115de4da91f61d58fcd12a45d97382d31ce
Samneet Singh <samneetdhillon@hotmail.com>
2021-03-14 20:54:50 -0400

    Check Balanced: Implement a function to check if a binary tree is balanced. For the purposes of this question, a balanced tree is defined to be a tree such that the heights of the two subtrees of any node never differ by more than one.

--- chapters/treesAndGraphs/isBalancedBinaryTree.py
+++ chapters/treesAndGraphs/isBalancedBinaryTree.py
@@ -3,7 +3,7 @@ Check Balanced: Implement a function to check if a binary tree is balanced. For 
 a balanced tree is defined to be a tree such that the heights of the two subtrees of any node never differ by
 more than one.
 """
-from chapters.dataStructures.binaryLeaf import BinaryLeaf
+from chapters.dataStructures.trees.binaryLeaf import BinaryLeaf
 
 
 def isBalancedBinaryTree(root: BinaryLeaf):

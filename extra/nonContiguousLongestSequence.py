599b0cc025b57aa4d0d14701b1ccc5f7d5af0602
Samneet Singh <samneetdhillon@hotmail.com>
2021-04-06 21:00:57 -0400

    Non Contiguous Longest Subsequence using tabulation technique of dynamic programming

--- extra/nonContiguousLongestSequence.py
+++ extra/nonContiguousLongestSequence.py
@@ -3,29 +3,37 @@ from typing import List
 
 def getNonContiguousLongestSubSequence(sequence: List) -> List:
     """
-    Time Complexity: O(n^3)
+    Time Complexity: O(n^2)
     Space Complexity: O(n)
     :param sequence: List
     :return: List
     """
-    longestSubSequence = []
-    for firstIndex in range(len(sequence)):
-        currentLongestSubSequence = []
+    lookup = [1] * len(sequence)
+    backtracking = dict()
+    longestSubsequenceLength = 0
+    largestIndex = 0
+    for j in range(1, len(sequence)):
+        currentLongestSubsequenceLength = lookup[j]
+        for i in range(j):
+            if sequence[i] < sequence[j]:
+                if lookup[i] + 1 > currentLongestSubsequenceLength:
+                    currentLongestSubsequenceLength = lookup[i] + 1
+                    backtracking[j] = i
+        lookup[j] = currentLongestSubsequenceLength
+        if currentLongestSubsequenceLength > longestSubsequenceLength:
+            longestSubsequenceLength = currentLongestSubsequenceLength
+            largestIndex = j
 
-        secondIndex = firstIndex + 1
-        thirdIndex = len(sequence)
-        while secondIndex < thirdIndex:
-            currentLongestSubSequence.append(sequence[firstIndex])
-            for fourthIndex in range(secondIndex, len(sequence)):
-                if sequence[fourthIndex] >= currentLongestSubSequence[-1]:
-                    currentLongestSubSequence.append(sequence[fourthIndex])
-            if len(currentLongestSubSequence) > len(longestSubSequence):
-                longestSubSequence = currentLongestSubSequence
-
-            currentLongestSubSequence = []
-            secondIndex += 1
-    return longestSubSequence
+    nextIndex = largestIndex
+    elements = []
+    while nextIndex in backtracking:
+        element = sequence[nextIndex]
+        elements.append(element)
+        nextIndex = backtracking[nextIndex]
+    elements.append(sequence[nextIndex])
+    return elements[::-1]
 
 
 if __name__ == '__main__':
+    print(getNonContiguousLongestSubSequence([1, 2, 3, 1, 4, 5, 2, 9, 3, 4, 5, 6, 7, 8, 9, 10]))
     print(getNonContiguousLongestSubSequence([5, 2, 10, 3, -1, 6, 8, 9, 3]))

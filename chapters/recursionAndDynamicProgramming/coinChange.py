0ed7114bc3603bf1506e093f0ca232d29ac0af77
samsing <samneetdhillon@hotmail.com>
2021-06-25 07:08:55 -0400

    Coins: Given an infinite number of quarters (25 cents), dimes (10 cents), nickels (5 cents), and pennies (1 cent), write code to calculate the number of ways of representing n cents. Hints: #300, #324, #343, #380, #394

--- chapters/recursionAndDynamicProgramming/coinChange.py
+++ chapters/recursionAndDynamicProgramming/coinChange.py
@@ -1,5 +1,5 @@
 """
-Coins: Given an innnite number of quarters (25 cents), dimes (10 cents), nickels (5 cents), and pennies (1 cent),
+Coins: Given an infinite number of quarters (25 cents), dimes (10 cents), nickels (5 cents), and pennies (1 cent),
 write code to calculate the number of ways of representing n cents.
 Hints: #300, #324, #343, #380, #394
 """
